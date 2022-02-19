32e664646d40bea61318b7e0c960c2dff7348303
Nikolay Zhmaev <nikolay.zhmaev85@gmail.com>
2022-02-19 16:15:33 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    // Timer
+
+    let deadline = '2022-02-20';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            // hours = Math.floor((t/1000/60/60) % 24),
+            // days = Math.floor (t/(1000*60*60*24));
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' :seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function addZero (num) {
+            if (num <=9) {
+                return '0' + num;
+            } else {
+                return num;
+            }
+        }
+
+        function updateClock () {
+            let t = getTimeRemaining(endtime);
+                hours.textContent = addZero(t.hours);
+                minutes.textContent = addZero(t.minutes);
+                seconds.textContent = addZero(t.seconds);
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
+});
